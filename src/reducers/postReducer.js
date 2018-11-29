0368da46488debee71ff5034975adc58fc7c86ab
patdevincentis <devincentis529@gmail.com>
2018-11-29 14:12:48 -0500

    added SET_RESULTS_POSTS reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,7 +1,7 @@
-import { SET_RECENT_POSTS } from "../actions/types";
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from "../actions/types";
 
 const INIT_STATE = {
-  posts: [],
+  resultsPosts: [],
   recentPosts: []
 };
 
@@ -11,7 +11,13 @@ export default function(state = INIT_STATE, action) {
       const recentPosts = action.payload;
 
       return { ...state, recentPosts };
+    case SET_RESULTS_POSTS:
+      const resultsPosts = action.payload;
 
+      return {
+        ...state,
+        resultsPosts
+      };
     default:
       return state;
   }
