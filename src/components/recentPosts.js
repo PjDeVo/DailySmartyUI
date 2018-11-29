55adf2255cf377e60610d080a91ee5537ee450ed
patdevincentis <devincentis529@gmail.com>
2018-11-28 21:50:04 -0500

    added post topics to post component

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -19,11 +19,12 @@ class RecentPosts extends Component {
     });
     return posts;
   };
+
   render() {
     return (
       <div className="recent-posts">
         <div className="recent-posts__wrapper">
-          <div className="recent-posts__heading"> Recent Posts </div>
+          <div className="recent-posts__heading">Recent Posts</div>
           <ul className="recent-posts__posts">{this.renderPosts()}</ul>
         </div>
       </div>
